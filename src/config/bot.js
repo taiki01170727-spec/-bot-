2ffd3f05eada330f22e78ee7987e0a5538002260
taiki01170727-spec <taiki.0117.0727@gmail.com>
2026-06-19 16:21:03 +0900

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Youtube ショート111を登録!!",
         // Activity type number (0 = Playing).
         type: 0,
       },
